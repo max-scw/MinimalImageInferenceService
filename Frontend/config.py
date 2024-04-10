6d915dcaaa5de15c08c133b3fa4950469ec665ca
SCHWMAX <max.schwenzer@voith.com>
2024-04-10 12:51:58 +0200

    make GENERAL_FILE_BOX_PATTERN optional

--- Frontend/config.py
+++ Frontend/config.py
@@ -105,9 +105,9 @@ def get_config_from_environment_variables() -> Tuple[ModelInfo, CameraInfo, AppS
         title=config["GENERAL_TITLE"] if "GENERAL_TITLE" in config else None,
         description=config["GENERAL_DESCRIPTION"] if "GENERAL_DESCRIPTION" in config else None,
         file_type_save_image=config["GENERAL_FILE_TYPE_SAVE_IMAGE"],
-        bbox_pattern=load_yaml(config["GENERAL_FILE_BOX_PATTERN"])
+        bbox_pattern=load_yaml(config["GENERAL_FILE_BOX_PATTERN"]) if "GENERAL_FILE_BOX_PATTERN" in config else None
     )
-    print(f"DEBUG config: {config}")
+
     return model_info, camera_info, app_settings
 
 
