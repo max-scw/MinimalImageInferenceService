ebe19156bcf3190ef3509adfa1922850ebc70ffb
SCHWMAX <max.schwenzer@voith.com>
2024-04-23 15:40:20 +0200

    widescreen layout and larger buttons

--- Frontend/app.py
+++ Frontend/app.py
@@ -3,6 +3,8 @@ import logging
 import numpy as np
 import sys
 
+# from streamlit_extras.stateful_button import button
+
 # custom packages
 from utils_streamlit import write_impress
 from utils_communication import trigger_camera, request_model_inference
@@ -11,7 +13,6 @@ from utils_coordinates import check_boxes
 from utils import get_env_variable, cast_logging_level
 from config import get_config_from_environment_variables, get_page_title
 from plot_pil import plot_bboxs
-
 
 @st.cache_data
 def get_config():
@@ -34,8 +35,24 @@ def reset_session_state_image():
 def main():
     st.set_page_config(
         page_title=get_page_title(),
-        page_icon=":camera:"
+        page_icon=":camera:",
+        layout="wide"
     )  #:rocket: # must be called as the first Streamlit command in your script.
+
+    # Custom CSS to style the buttons
+    st.markdown("""
+        <style>
+            button {
+                padding-top: 50px !important;
+                padding-bottom: 50px !important;
+            }
+            toggle {
+                padding-top: 50px !important;
+                padding-bottom: 50px !important;
+                width: 180px !important;
+        }
+        </style>
+    """, unsafe_allow_html=True)
 
     # load configs
     model_info, camera_info, app_settings = get_config()
@@ -55,20 +72,21 @@ def main():
         st.write(app_settings.description)
 
     # ----- buttons
-    columns = st.columns([2, 1, 1])
+    columns = st.columns([1, 1, 1, 1])
 
     with columns[0]:
         camera_triggered = st.button(
-            "Trigger",
+            r"$\textsf{\Large Trigger}$",
             help="trigger camera and call model.",
-            type="primary"
+            type="primary",
+            use_container_width=True
         )
         if camera_triggered:
             st.session_state.buttons_disabled = False
 
-    with columns[1]:
+    with columns[2]:
         toggle_boxes = st.toggle(
-            "show boxes",
+            r"$\textsf{\Large show boxes}$",
             value=camera_triggered,
             help="Toggles bounding-boxes.",
             disabled=st.session_state.buttons_disabled
@@ -76,12 +94,13 @@ def main():
         if toggle_boxes:
             st.session_state.show_bboxs = not st.session_state.show_bboxs
 
-    with columns[2]:
+    with columns[3]:
         overrule_decision = st.button(
-            "Overrule decision",
+            r"$\textsf{\Large Overrule decision}$",
             help="Flags the image as wrongly classified",
             type="secondary",
-            disabled=st.session_state.buttons_disabled or st.session_state.image["overruled"]
+            disabled=st.session_state.buttons_disabled or st.session_state.image["overruled"],
+            use_container_width=True
         )
 
     # processing
