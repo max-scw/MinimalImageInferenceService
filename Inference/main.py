cc7afc10fd882992f42eec83788f4b182dc3b176
SCHWMAX <max.schwenzer@voith.com>
2024-08-14 09:17:18 +0200

    consistent logging

--- Inference/main.py
+++ Inference/main.py
@@ -10,29 +10,28 @@ import logging
 from timeit import default_timer
 
 # custom packages
-from utils import get_config, get_logging_level
+from utils import get_config, setup_logging
 from utils_fastapi import default_fastapi_setup
 from utils_image import scale_coordinates_to_image_size, prepare_image, image_from_bytes
 
-# set logging level
-LOG_LEVEL = get_logging_level(default=logging.DEBUG)
-logging.getLogger().setLevel(LOG_LEVEL)
+# Setup logging
+logger = setup_logging(__name__)
 
 # get config
 CONFIG = get_config()
-logging.debug(f"Configuration (CONFIG): {CONFIG}")
+logger.debug(f"Configuration (CONFIG): {CONFIG}")
 
 # get model path from config
 model_path = Path(CONFIG["MODEL_FOLDER_DATA"]) / CONFIG["MODEL_FILENAME"]
 path_to_model_file = model_path.with_suffix(".onnx")
-logging.info(f"Loading model from {path_to_model_file} (file exists: {path_to_model_file.exists()})")
+logger.info(f"Loading model from {path_to_model_file} (file exists: {path_to_model_file.exists()})")
 
 # initialize ONNX session
 ONNX_SESSION = ort.InferenceSession(path_to_model_file)
 
 # log input shapes
 input_shapes = {el.name: el.shape for el in ONNX_SESSION.get_inputs()}
-logging.debug(f"Model input(s) {input_shapes}")
+logger.debug(f"Model input(s) {input_shapes}")
 
 # entry points
 ENTRYPOINT_INFERENCE = "/inference"
@@ -45,9 +44,7 @@ app = default_fastapi_setup(title, summary)
 
 @app.post(ENTRYPOINT_INFERENCE)
 async def predict(image: UploadFile = File(...)):
-    # get local logger + set logging level
-    logging.getLogger().setLevel(LOG_LEVEL)
-    logging.debug(f"call {ENTRYPOINT_INFERENCE}")
+    logger.debug(f"call {ENTRYPOINT_INFERENCE}")
 
     if image.content_type.split("/")[0] != "image":
         raise HTTPException(status_code=400, detail="Uploaded file is not an image.")
@@ -55,26 +52,26 @@ async def predict(image: UploadFile = File(...)):
     # wait for file transmission
     image_bytes = await image.read()
     img = image_from_bytes(image_bytes)
-    logging.debug(f"Image received: {img.shape}")
+    logger.debug(f"Image received: {img.shape}")
 
     # preprocess image
     img_mdl = prepare_image(img, CONFIG["MODEL_IMAGE_SIZE"], CONFIG["MODEL_PRECISION"])
-    logging.debug(f"Image shape, config: {CONFIG['MODEL_IMAGE_SIZE']}, prepared {img_mdl.shape}")
+    logger.debug(f"Image shape, config: {CONFIG['MODEL_IMAGE_SIZE']}, prepared {img_mdl.shape}")
 
     t0 = default_timer()
     input_name = ONNX_SESSION.get_inputs()[0].name
     output_name = ONNX_SESSION.get_outputs()[0].name
     results = ONNX_SESSION.run([output_name], {input_name: img_mdl})
-    logging.debug(f"Inference took {(default_timer() - t0) / 1000:.2g} ms.")
+    logger.debug(f"Inference took {(default_timer() - t0) / 1000:.2g} ms.")
 
-    logging.debug(f"len(results)={len(results)}; results[0].shape={results[0].shape}")
+    logger.debug(f"len(results)={len(results)}; results[0].shape={results[0].shape}")
 
     bboxes = results[0][:, 1:5]
     class_ids = results[0][:, 5]
     scores = results[0][:, 6]
 
     # re-scale boxes
-    logging.debug(f"Rescale boxes to original image size: img_mdl.shape={img_mdl.shape}, img.shape={img.shape}")
+    logger.debug(f"Rescale boxes to original image size: img_mdl.shape={img_mdl.shape}, img.shape={img.shape}")
     bboxes = scale_coordinates_to_image_size(bboxes, img_mdl.shape[2:], img.shape[:2])
 
     content = {
@@ -86,11 +83,9 @@ async def predict(image: UploadFile = File(...)):
 
 
 if __name__ == "__main__":
-    log_level = get_logging_level(default=logging.DEBUG)
-    # get logger
-    logger = logging.getLogger("uvicorn")
-    logger.setLevel(log_level)
-    logger.debug(f"logger.level={logger.level}")
-
-    logger.debug("====> Starting uvicorn server <====")
-    uvicorn.run(app=app, port=5052, log_level=log_level)
+    uvicorn.run(
+        app=app,
+        port=5052,
+        access_log=True,
+        log_config=None  # Uses the logging configuration in the application
+    )
