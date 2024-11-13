526bf123996cc7c9eb1c44ffd0f2492149236dfd
SCHWMAX <max.schwenzer@voith.com>
2024-11-13 14:47:31 +0100

    fix default values in config

--- utils_config.py
+++ utils_config.py
@@ -11,7 +11,7 @@ def get_basler_camera_parameter_from_config(config: Dict[str, Any]) -> BaslerCam
 
     camera_info = BaslerCameraSettings(
         # camera addresses
-        serial_number=config["CAMERA_SERIAL_NUMBER"] if isinstance(config["CAMERA_SERIAL_NUMBER"], int) else None,
+        serial_number=config["CAMERA_SERIAL_NUMBER"] if ("CAMERA_SERIAL_NUMBER" in config) and isinstance(config["CAMERA_SERIAL_NUMBER"], int) else None,
         ip_address=config["CAMERA_IP_ADDRESS"] if "CAMERA_IP_ADDRESS" in config else None,
         subnet_mask=config["CAMERA_SUBNET_MASK"] if "CAMERA_SUBNET_MASK" in config else None,
         # camera communication
@@ -24,27 +24,6 @@ def get_basler_camera_parameter_from_config(config: Dict[str, Any]) -> BaslerCam
     )
 
     return camera_info
-
-
-# def get_photo_parameter_from_config(config: Dict[str, Any]) -> PhotoParams:
-#     photo_params = PhotoParams(
-#         # exposure_time_microseconds: Optional[int] = 10000
-#         # communication
-#         timeout=config["CAMERA_TIMEOUT"] if isinstance(config["CAMERA_TIMEOUT"], int) else None,
-#         # debugging
-#         emulate_camera=config["CAMERA_EMULATE_CAMERA"] if "CAMERA_EMULATE_CAMERA" in config else None,
-#         # image parameter
-#         **get_image_parameter_from_config(config).model_dump()
-# )
-#     # set exposure time if environment variable exists and has a valid format
-#     exposure_time_microseconds = None
-#     for ky in ["CAMERA_EXPOSURE_TIME", "CAMERA_EXPOSURE_TIME_MICROSECONDS"]:
-#         if (ky in config) and isinstance(config[ky], int):
-#             exposure_time_microseconds = config[ky]
-#     if exposure_time_microseconds:
-#         photo_params.exposure_time_microseconds = exposure_time_microseconds
-#
-#     return photo_params
 
 
 def get_image_parameter_from_config(config: Dict[str, Any]) -> ImageParams:
